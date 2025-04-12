5b829e65b9106865c97e1a33fd513be175853fb0
sam <samchimaster@gmail.com>
2025-04-12 23:06:16 +0100

    Fixes route

--- apps/pixel-dog/src/App.tsx
+++ apps/pixel-dog/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import HelloWorld from './views/HelloWorld';
+// import HelloWorld from './views/HelloWorld';
 import PixelArtView from './views/PixelArtView';
 
 const App = () => {
@@ -8,13 +8,13 @@ const App = () => {
     <div>
       <nav className="bg-gray-800 p-4">
         <div className="max-w-4xl mx-auto flex gap-4">
-          <Link to="/" className="text-white hover:text-gray-300">Counter</Link>
-          <Link to="/pixel-art" className="text-white hover:text-gray-300">Pixel Art Dogs</Link>
+          {/* <Link to="/" className="text-white hover:text-gray-300">Counter</Link> */}
+          <Link to="/" className="text-white hover:text-gray-300">Pixel Art Dogs</Link>
         </div>
       </nav>
       <Routes>
-        <Route path="/" element={<HelloWorld />} />
-        <Route path="/pixel-art" element={<PixelArtView />} />
+        {/* <Route path="/" element={<HelloWorld />} /> */}
+        <Route path="/" element={<PixelArtView />} />
       </Routes>
     </div>
   );
